0964aa421733c365427bc2722e9c363019ed2ee7
Nicola Coretti <nico.coretti@gmail.com>
2015-12-10 17:30:41 +0100

    ---/ updated requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sirtalkalot',
     version='0.1.0',
-    packages=['tests', 'sirtalkalot'],
+    packages=find_packages(),
     install_requires=['docopt', 'libslack', 'ws4py'],
     url='https://github.com/Nicoretti/sirtalkalot',
     license='BSD',
